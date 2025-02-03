77cbfe4edd5dbb8ce8707d2b625dbe2f72d6cb08
luisflr <lfloresr@ulasalle.edu.pe>
2025-02-02 20:45:25 -0500

    added syles to 'Our Products'

--- src/Views/HomePage/ProjectsView.tsx
+++ src/Views/HomePage/ProjectsView.tsx
@@ -1,22 +1,32 @@
+import Vitrina from "../../assets/vitrina-70-a.jpg"
+import MuroCortina from "../../assets/muro-cortina.jpg"
+import Moduglass from "../../assets/ventana.jpg"
+
 function ProjectsView() {
   return (
     <section className='w-full flex flex-col justify-center items-center py-20'>
       <h1 className='text-5xl text-cyan-950 font-bold mb-16'> Nuestros Productos </h1>
       <div className='w-full px-20 grid grid-cols-3 gap-8'>
-        <button className='w-full relative h-[700px]  bg-[url("assets/vitrina-70-a.jpg")] bg-cover'>
-          <div className="absolute w-full h-full bg-black opacity-40 z-0 top-0">
-          </div>
-          <p className='absolute z-20 text-2xl font-bold text-white text-center w-full'>Carpinteria en aluminio</p>
+        <button className='group w-full relative h-[700px] overflow-hidden bg-black'>
+          <img 
+            src={Vitrina}
+            className="absolute top-0 object-cover w-full h-full opacity-25 group-hover:scale-110 group-hover:opacity-65 transition-all duration-500 ease-in-out"
+          />
+          <p className='absolute z-20 text-2xl font-bold text-white text-center w-full group-hover:scale-110 transition transform duration-1000'>Carpinteria en aluminio</p>
         </button>
-        <button className='w-full relative h-[700px]  bg-[url("assets/muro-cortina.jpg")] bg-cover'>
-          <div className="absolute w-full h-full bg-black opacity-40 z-0 top-0">
-          </div>
-          <p className='absolute z-20 text-2xl font-bold text-white text-center w-full'>Muro Cortina</p>
+        <button className='group w-full relative h-[700px] overflow-hidden bg-black'>
+          <img 
+            src={MuroCortina}
+            className="absolute top-0 object-cover w-full h-full opacity-25 group-hover:scale-110 group-hover:opacity-65 transition-all duration-500 ease-in-out"
+          />
+          <p className='absolute z-20 text-2xl font-bold text-white text-center w-full group-hover:scale-110 transition transform duration-1000'>Muro Cortina</p>
         </button>
-        <button className='w-full relative h-[700px]  bg-[url("assets/ventana.jpg")] bg-cover'>
-          <div className="absolute w-full h-full bg-black opacity-40 z-0 top-0">
-          </div>
-          <p className='absolute z-20 text-2xl font-bold text-white text-center w-full'>Sistema Moduglass</p>
+        <button className='group w-full relative h-[700px] overflow-hidden bg-black'>
+          <img 
+            src={Moduglass}
+            className="absolute top-0 object-cover w-full h-full opacity-25 group-hover:scale-110 group-hover:opacity-65 transition-all duration-500 ease-in-out"
+          />
+          <p className='absolute z-20 text-2xl font-bold text-white text-center w-full group-hover:scale-110 transition transform duration-1000'>Sistema Moduglass</p>
         </button>
       </div>
     </section>
